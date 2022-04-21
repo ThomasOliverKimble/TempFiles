1e0bfb4a4008ee380e7db02710aca9e0a891e92a
ThomasOliverKimble <tokimble@gmail.com>
2022-04-21 08:35:20 +0200

    added simulations

---
+++ simulations/controllers/accelerometer/build/release/accelerometer.d
@@ -0,0 +1,7 @@
+build/release/accelerometer.o: accelerometer.c \
+ /home/thomas/webots/include/controller/c/webots/accelerometer.h \
+ /home/thomas/webots/include/controller/c/webots/types.h \
+ /home/thomas/webots/include/controller/c/webots/led.h \
+ /home/thomas/webots/include/controller/c/webots/motor.h \
+ /home/thomas/webots/include/controller/c/webots/robot.h \
+ /home/thomas/webots/include/controller/c/webots/nodes.h
